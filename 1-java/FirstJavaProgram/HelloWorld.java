f3d54c7bce6bc35b92a29ff29e849799a1a78f26
bryanserfozo <bryan.serfozo@revature.com>
2022-06-13 17:43:38 -0400

    Monday's Notes

---
+++ 1-java/FirstJavaProgram/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld{
+
+	public static void main(String[] args){
+
+		// This is a comment
+		System.out.println("Hello World!");
+		printMessage();
+
+	}
+
+	public static void printMessage(){
+
+		System.out.println("Second Method");
+
+	}
+
+
+}
