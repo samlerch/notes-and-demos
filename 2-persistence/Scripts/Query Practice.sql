3c565c7b7ae99810e6ec4c0cc2beb0046ccb228b
bryanserfozo <bryan.serfozo@revature.com>
2022-06-20 13:59:54 -0400

    Adding notes from morning of 6/20

---
+++ 2-persistence/Scripts/Query Practice.sql
@@ -0,0 +1,142 @@
+-- Use ctrl + a to select everything and then ctrl + enter to run everything at once to save time
+
+create table employee (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	department VARCHAR(50)
+);
+insert into employee (id, first_name, last_name, email, department) values (1, 'Millisent', 'Minucci', 'mminucci0@yahoo.co.jp', 'Services');
+insert into employee (id, first_name, last_name, email, department) values (2, 'Cristina', 'Rosenstengel', 'crosenstengel1@unesco.org', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (3, 'Desmund', 'Gadman', 'dgadman2@taobao.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (4, 'Hally', 'McBrearty', 'hmcbrearty3@fc2.com', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (5, 'Jordan', 'Brainsby', 'jbrainsby4@yellowpages.com', 'Product Management');
+insert into employee (id, first_name, last_name, email, department) values (6, 'Hildagarde', 'Sellens', 'hsellens5@examiner.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (7, 'Doloritas', 'Grimsdale', 'dgrimsdale6@arizona.edu', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (8, 'Noble', 'Noble', 'nnoble7@boston.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (9, 'Kellby', 'Riehm', 'kriehm8@prlog.org', 'Training');
+insert into employee (id, first_name, last_name, email, department) values (10, 'Britta', 'Grinnov', 'bgrinnov9@globo.com', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (11, 'Mose', 'Prowting', 'mprowtinga@forbes.com', 'Product Management');
+insert into employee (id, first_name, last_name, email, department) values (12, 'Danie', 'Dmitrienko', 'ddmitrienkob@state.gov', 'Training');
+insert into employee (id, first_name, last_name, email, department) values (13, 'Hilliary', 'Nunnerley', 'hnunnerleyc@hc360.com', 'Support');
+insert into employee (id, first_name, last_name, email, department) values (14, 'Geralda', 'Manketell', 'gmanketelld@dion.ne.jp', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (15, 'Tully', 'Elwood', 'telwoode@ask.com', 'Services');
+insert into employee (id, first_name, last_name, email, department) values (16, 'Bunni', 'Cowl', 'bcowlf@list-manage.com', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (17, 'Maisie', 'Highnam', 'mhighnamg@businessinsider.com', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (18, 'Karim', 'Jowle', 'kjowleh@miibeian.gov.cn', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (19, 'Madge', 'Britt', 'mbritti@free.fr', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (20, 'Renell', 'Ganiford', 'rganifordj@merriam-webster.com', 'Legal');
+insert into employee (id, first_name, last_name, email, department) values (21, 'Sunny', 'Cragoe', 'scragoek@flickr.com', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (22, 'Ruthanne', 'Tomkinson', 'rtomkinsonl@ovh.net', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (23, 'Arlyn', 'Vlasov', 'avlasovm@multiply.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (24, 'Edita', 'Medwell', 'emedwelln@ocn.ne.jp', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (25, 'Nancie', 'Alsobrook', 'nalsobrooko@chronoengine.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (26, 'Emilio', 'Moverley', 'emoverleyp@cmu.edu', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (27, 'Adoree', 'Duffil', 'aduffilq@yelp.com', 'Legal');
+insert into employee (id, first_name, last_name, email, department) values (28, 'Derry', 'Redfearn', 'dredfearnr@usnews.com', 'Product Management');
+insert into employee (id, first_name, last_name, email, department) values (29, 'Reagan', 'Shambrook', 'rshambrooks@jigsy.com', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (30, 'Natalina', 'Heinz', 'nheinzt@adobe.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (31, 'Wes', 'Greader', 'wgreaderu@gizmodo.com', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (32, 'Holly', 'Brocking', 'hbrockingv@cisco.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (33, 'Isabella', 'McAree', 'imcareew@fotki.com', 'Support');
+insert into employee (id, first_name, last_name, email, department) values (34, 'Regen', 'McGeraghty', 'rmcgeraghtyx@unblog.fr', 'Legal');
+insert into employee (id, first_name, last_name, email, department) values (35, 'Todd', 'Gowlett', 'tgowletty@timesonline.co.uk', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (36, 'Dolly', 'Baly', 'dbalyz@soundcloud.com', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (37, 'Albie', 'Buckleigh', 'abuckleigh10@chicagotribune.com', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (38, 'Gage', 'Torvey', 'gtorvey11@yale.edu', 'Accounting');
+insert into employee (id, first_name, last_name, email, department) values (39, 'Lennard', 'Ogger', 'logger12@mlb.com', 'Training');
+insert into employee (id, first_name, last_name, email, department) values (40, 'Anna', 'Kingett', 'akingett13@booking.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (41, 'Estel', 'Sherel', 'esherel14@sciencedaily.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (42, 'Hewet', 'Mayor', 'hmayor15@reuters.com', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (43, 'Laurie', 'Dovermann', 'ldovermann16@icq.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (44, 'Garvey', 'Heinzel', 'gheinzel17@abc.net.au', 'Accounting');
+insert into employee (id, first_name, last_name, email, department) values (45, 'Gonzales', 'Huggard', 'ghuggard18@gnu.org', 'Training');
+insert into employee (id, first_name, last_name, email, department) values (46, 'Wildon', 'Leimster', 'wleimster19@senate.gov', 'Services');
+insert into employee (id, first_name, last_name, email, department) values (47, 'Freddie', 'Wistance', 'fwistance1a@wunderground.com', 'Support');
+insert into employee (id, first_name, last_name, email, department) values (48, 'Lenora', 'Barkway', 'lbarkway1b@creativecommons.org', 'Services');
+insert into employee (id, first_name, last_name, email, department) values (49, 'Dianemarie', 'Maddyson', 'dmaddyson1c@irs.gov', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (50, 'Fredi', 'McCluskey', 'fmccluskey1d@mlb.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (51, 'Emelyne', 'Detoc', 'edetoc1e@craigslist.org', 'Legal');
+insert into employee (id, first_name, last_name, email, department) values (52, 'Liva', 'Troth', 'ltroth1f@t-online.de', 'Support');
+insert into employee (id, first_name, last_name, email, department) values (53, 'Simon', 'Kleinsinger', 'skleinsinger1g@angelfire.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (54, 'Gideon', 'Catley', 'gcatley1h@icio.us', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (55, 'Leta', 'Cleaver', 'lcleaver1i@technorati.com', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (56, 'Raoul', 'Jeratt', 'rjeratt1j@howstuffworks.com', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (57, 'Finn', 'Arkwright', 'farkwright1k@t-online.de', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (58, 'Nana', 'Gronaller', 'ngronaller1l@statcounter.com', 'Accounting');
+insert into employee (id, first_name, last_name, email, department) values (59, 'Emmanuel', 'McDirmid', 'emcdirmid1m@tuttocitta.it', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (60, 'Vivyan', 'Braunthal', 'vbraunthal1n@theguardian.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (61, 'Garrott', 'Valentin', 'gvalentin1o@intel.com', 'Training');
+insert into employee (id, first_name, last_name, email, department) values (62, 'Josey', 'Sapsforde', 'jsapsforde1p@theglobeandmail.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (63, 'Davidde', 'Hawse', 'dhawse1q@bbc.co.uk', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (64, 'Jerry', 'Brayson', 'jbrayson1r@comcast.net', 'Product Management');
+insert into employee (id, first_name, last_name, email, department) values (65, 'Joshua', 'Shuttell', 'jshuttell1s@mashable.com', 'Support');
+insert into employee (id, first_name, last_name, email, department) values (66, 'Dalton', 'Besnardeau', 'dbesnardeau1t@deliciousdays.com', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (67, 'Willy', 'Rumble', 'wrumble1u@google.es', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (68, 'Phylis', 'Loche', 'ploche1v@example.com', 'Product Management');
+insert into employee (id, first_name, last_name, email, department) values (69, 'Caron', 'MacVaugh', 'cmacvaugh1w@addthis.com', 'Legal');
+insert into employee (id, first_name, last_name, email, department) values (70, 'Keene', 'Feragh', 'kferagh1x@statcounter.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (71, 'Ulrica', 'Manning', 'umanning1y@europa.eu', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (72, 'Eulalie', 'Kiely', 'ekiely1z@oracle.com', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (73, 'Neel', 'Orvis', 'norvis20@msu.edu', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (74, 'Rinaldo', 'Bungey', 'rbungey21@behance.net', 'Support');
+insert into employee (id, first_name, last_name, email, department) values (75, 'Maybelle', 'Gyenes', 'mgyenes22@spiegel.de', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (76, 'Essy', 'Tidswell', 'etidswell23@webeden.co.uk', 'Training');
+insert into employee (id, first_name, last_name, email, department) values (77, 'Viviana', 'Burgot', 'vburgot24@bbb.org', 'Accounting');
+insert into employee (id, first_name, last_name, email, department) values (78, 'Shana', 'Morrill', 'smorrill25@simplemachines.org', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (79, 'Isahella', 'Lemasney', 'ilemasney26@about.com', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (80, 'Claretta', 'Crammy', 'ccrammy27@dagondesign.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (81, 'Edmon', 'Spare', 'espare28@dailymotion.com', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (82, 'Daloris', 'Gudeman', 'dgudeman29@mapquest.com', 'Accounting');
+insert into employee (id, first_name, last_name, email, department) values (83, 'Burgess', 'Pardey', 'bpardey2a@nsw.gov.au', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (84, 'Elyn', 'Bradborne', 'ebradborne2b@ted.com', 'Legal');
+insert into employee (id, first_name, last_name, email, department) values (85, 'Paloma', 'Trevillion', 'ptrevillion2c@google.cn', 'Human Resources');
+insert into employee (id, first_name, last_name, email, department) values (86, 'Konstanze', 'Goater', 'kgoater2d@microsoft.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (87, 'Archibald', 'de Merida', 'ademerida2e@ezinearticles.com', 'Accounting');
+insert into employee (id, first_name, last_name, email, department) values (88, 'Katey', 'Croydon', 'kcroydon2f@cloudflare.com', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (89, 'Fonsie', 'Ruddiman', 'fruddiman2g@clickbank.net', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (90, 'Xymenes', 'Dencs', 'xdencs2h@kickstarter.com', 'Services');
+insert into employee (id, first_name, last_name, email, department) values (91, 'Jean', 'Waterhous', 'jwaterhous2i@xing.com', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (92, 'Jesselyn', 'Eastope', 'jeastope2j@fda.gov', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (93, 'Reuven', 'Frowd', 'rfrowd2k@wikimedia.org', 'Sales');
+insert into employee (id, first_name, last_name, email, department) values (94, 'Muriel', 'Hourstan', 'mhourstan2l@oakley.com', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (95, 'Ebonee', 'Bitchener', 'ebitchener2m@craigslist.org', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (96, 'Avictor', 'Woodyear', 'awoodyear2n@about.com', 'Research and Development');
+insert into employee (id, first_name, last_name, email, department) values (97, 'Gil', 'Rydeard', 'grydeard2o@illinois.edu', 'Engineering');
+insert into employee (id, first_name, last_name, email, department) values (98, 'Jammie', 'Petyanin', 'jpetyanin2p@foxnews.com', 'Marketing');
+insert into employee (id, first_name, last_name, email, department) values (99, 'Vanna', 'Keenan', 'vkeenan2q@vinaora.com', 'Business Development');
+insert into employee (id, first_name, last_name, email, department) values (100, 'Thorpe', 'Vanyushin', 'tvanyushin2r@unicef.org', 'Engineering');
+
+-- Insert a new entry for the 101st employee Lexie Vail, her email is lvail0@nyu.edu and she works in Legal
+
+-- Insert a new entry for the 102nd employee Horton Farrow, his email is hfarrow1@answers.com and he works in Services
+
+-- Insert a new entry for the 103rd employee Pati Ulster, her email is pulster2@macromedia.com and she works in Sales
+
+-- Update the department of the employee with the id of 45 to Legal
+
+-- Update the first and last name of employee number 12 to Jacob Banfield 
+
+-- Delete any employees with the last name Britt
+
+-- Delete any employees in the services department
+
+-- Select all entries
+
+-- Select all first names
+
+-- Select all employees with an ID greater than or equal to 75
+
+-- Select any employees from the Research and Development department
+
+-- Select all employees with an email that ends in .edu
+
+-- Delete all employees with an email that ends in .edu
+
+-- Select all employees whose first name ends in 'e' or whose last name starts with 'e'
+
+-- Select first and last names for all remaining employees, order it alphabetically by last name 
+
+-- Select all entries for employees with a department that starts with 'a', order them by id from largest to smallest
+
